3da6647c56d9d164880bb8016bf7acabc32df690
Daegonica <memphis1988s@gmail.com>
2025-12-22 19:14:31 -0700

    Adding basic Twitch Chatbot. Can track/send messages via terminal.

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use twitch_irc::{
+    login::{TokenStorage, UserAccessToken, RefreshingLoginCredentials},
+    ClientConfig,
+    SecureTCPTransport,
+    TwitchIRCClient,
+    message::ServerMessage,
+};
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::sync::broadcast;
+use async_trait::async_trait;
+use std::{fs, env};
+use std::io::{self as stdio, Write};
+use dotenv::dotenv;
+use dlog::{*, enums::OutputTarget};
+// Initiate logger with 'let mut log = Logger::init('app_name', None, OutputTarget::(Terminal/LogFile)).unwrap();'
+
+#[derive(Debug)]
+struct FileTokenStorage {
+    path: String,
+}
+
+#[async_trait]
+impl TokenStorage for FileTokenStorage {
+
+    type LoadError = std::io::Error;
+    type UpdateError = std::io::Error;
+
+    async fn load_token(&mut self) -> Result<UserAccessToken, Self::LoadError> {
+        let data = fs::read_to_string(&self.path)?;
+        let token: UserAccessToken = serde_json::from_str(&data)?;
+        Ok(token)
+    }
+
+    async fn update_token(&mut self, token: &UserAccessToken) -> Result<(), Self::UpdateError> {
+        let data = serde_json::to_string(token)?;
+        fs::write(&self.path, data)?;
+        Ok(())
+    }
+}
+
+
+#[tokio::main]
+pub async fn main() {
+
+    let mut log = Logger::init("twitch_bot", None, OutputTarget::Terminal).unwrap();
+
+    dotenv().ok();
+    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
+    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
+    let twitch_channel = env::var("TWITCH_CHANNEL").expect("TWITCH_CHANNEL not set");
+
+    let storage = FileTokenStorage { path: "token.json".to_string() };
+
+    let credentials = RefreshingLoginCredentials::init(client_id, client_secret, storage);
+    let config = ClientConfig::new_simple(credentials);
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, RefreshingLoginCredentials<FileTokenStorage>>::new(config);
+
+
+    // Create a shutdown signal channel
+    let (shutdown_tx, mut shutdown_rx) = broadcast::channel::<()>(1);
+
+    let join_handle = tokio::spawn({
+        let mut shutdown_rx = shutdown_tx.subscribe();
+        async move {
+            loop {
+                tokio::select! {
+                    maybe_msg = incoming_messages.recv() => {
+                        match maybe_msg {
+                            Some(message) => match message {
+                                ServerMessage::Privmsg(msg) => {
+                                    log.info(format!("[{}]: {}", msg.sender.name, msg.message_text));
+                                },
+                                ServerMessage::Whisper(msg) => {
+                                    log.info(format!("(w) {}: {}", msg.sender.name, msg.message_text));
+                                },
+                                _ => {}
+                            },
+                            None => break,
+                        }
+                    }
+                    _ = shutdown_rx.recv() => {
+                        // Received shutdown signal
+                        break;
+                    }
+                }
+            }
+        }
+    });
+
+    client.join(twitch_channel.clone()).unwrap();
+    
+
+    let client_clone = client.clone();
+    let shutdown_tx2 = shutdown_tx.clone();
+    let twitch_channel2 = twitch_channel.clone();
+    tokio::spawn(async move {
+        let stdin = io::stdin();
+        let mut reader = BufReader::new(stdin).lines();
+        loop {
+            print!("> ");
+            stdio::stdout().flush().unwrap();
+
+            match reader.next_line().await {
+                Ok(Some(line)) if !line.trim().is_empty() => {
+                    if line.trim() == "!quit" {
+                        let _ = client_clone.part(twitch_channel2.clone());
+                        let _ = shutdown_tx2.send(()); // Signal shutdown
+                        break;
+                    } else {
+                        let _ = client_clone.say(twitch_channel2.clone(), line).await;
+                    }
+                }
+                Ok(Some(_)) => continue,
+                Ok(None) | Err(_) => break,
+            }
+        }
+    });
+
+    join_handle.await.unwrap();
+}
